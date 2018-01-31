3cc077e5a7cbdcfb422b33bb9d70d93fdc329d27
Davide Rizzi <sadjehwty@gmail.com>
2018-01-31 21:44:35 +0100

    favicon e version

--- ch.rb
+++ ch.rb
@@ -7,8 +7,19 @@ get '/' do
   '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Circle Histogram</title></head><body><code>curl -X PUT -d @myfilename https://circlehisto.herokuapp.com</code> or <form method="POST" action="" enctype="multipart/form-data"><input type="file" name="file" /><input type="submit" /></form></body></html>'
 end
 
+get '/version' do
+  content_type 'application/json'
+  '{"version": "0.3.0"}'
+end
+
+get '/favicon.ico' do
+  content_type 'image/x-icon'
+  send_file  'favicon.ico'
+end
+
 post '/' do
   img=Magick::Image.from_blob(params[:file][:tempfile].read).first
+  headers['Content-Disposition'] = 'attachment; filename="response.png"'
   circle_histogram img
 end
 
